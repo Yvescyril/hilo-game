3ad6211a85fb49c7ae33ce61cc9cb39461f05e49
Yvescyril <cyrilyves61@gmail.com>
2022-02-01 23:07:58 -0500

    assignment for week 4 it's not yet but will be

---
+++ README.md
@@ -0,0 +1,2 @@
+# The hilo game seems to be a great game every time you play it and then 
+# you foresee the right outcome you wilñ win 100 points as well as you guess wrong you lose 75 point
---
+++ hilo-game.py
@@ -0,0 +1,21 @@
+
+from random import random
+
+
+
+class hilo:
+    # each card stands for a digit from 1 to 13
+    # the role of hilo is to determine whether the value of card hiher or
+    # lower but still from the same range
+
+    def __init__(Self):
+        # builds the instance for the hilo
+        Self.cardvalue = 0
+    def cardPoint(self):
+        #generating value and then compute the points
+        self.cardPoint= random.randint(1,13)
+class refree:
+    # that's just the one who is going to lead the game
+    def __init_(self):
+        pass
+        self.card1= 0
